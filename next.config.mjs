9a6b211b4a8f57288b586850b1b809cec258f9ec
Tolemy Kashyap <kashyaptolemy@gmail.com>
2024-02-09 16:55:45 +0530

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+// next.config.mjs
+export default {
+    experimental: {
+      appDir: true
+    }
+  };
+  
