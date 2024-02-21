2ce54b6755719bcf9148c96a79662217b96f3460
Tolemy Kashyap <kashyaptolemy@gmail.com>
2024-02-21 18:16:45 +0530

    css changes

--- src/style-guide/components/Intro/index.jsx
+++ src/style-guide/components/Intro/index.jsx
@@ -12,7 +12,7 @@ const Intro = () => {
             <div className={styles.intro_container}>
                 <div className={styles.name}>Tolemy Kashyap</div>
                 <div className={styles.position}>Web Developer & Designer</div>
-                <div className={styles.small_intro}>I build (well atleast try to) visually appealing web applications.</div>
+                <div className={styles.small_intro}>I build (well atleast try to) visually appealing web apps for my survival.</div>
                 <Navbar />
                 <Resume />
                 <SocialSection />
